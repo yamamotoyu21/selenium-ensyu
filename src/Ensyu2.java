01eab4389a24ba0e3d748e564850bf289e68dda1
yamamotoyu <yu.yamamoto@rakus-partners.co.jp>
2021-05-21 14:51:22 +0900

    演習2完了

--- src/Ensyu2.java
+++ src/Ensyu2.java
@@ -1,11 +1,29 @@
+
+
+
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+
 import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+
+
+
+
+
 
 public class Ensyu2 {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
 		
 		System.setProperty("webdriver.chrome.driver", "./exe/chromedriver");
@@ -19,7 +37,11 @@ public class Ensyu2 {
 		password.sendKeys("yu.yamamoto@rakus-partners.co.jp");
 		
 		userName.submit();
-		password.submit();
+		//password.submit();
+
+		File file = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+		Files.copy(file.toPath(), new File("/Users/yamamotoyuu/Documents/workspace/Selenium-ensyu" + file.getName()).toPath());
+		
 		
 		driver.quit();
 		
